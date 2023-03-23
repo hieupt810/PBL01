2cce6ca0f6b042f3ea1d02e037ad62fae4b1b37b
hieupt <trunghieupham03@gmail.com>
2023-03-23 13:07:29 +0700

    #

--- algorithm/priority_queue.cpp
+++ algorithm/priority_queue.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void init() {
+void init()
+{
     freopen("Input.inp", "r", stdin);
     freopen("Output.out", "w", stdout);
 }
@@ -10,22 +11,27 @@ priority_queue<int> q;
 long long d;
 int n, k, res = 0;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     init();
     cin >> n >> d >> k;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         int x;
         cin >> x;
         q.push(x);
     }
-    while (d > 0 && ++res <= k + 1) {
+    while (d > 0 && ++res <= k + 1)
+    {
         int x = q.top();
         d -= x;
         q.pop();
         q.push(x / 2);
     }
-    if (res <= k && d <= 0) cout << res;
-    else cout << -1;
+    if (res <= k && d <= 0)
+        cout << res;
+    else
+        cout << -1;
     return 0;
 }
--- algorithm/sort.cpp
+++ algorithm/sort.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void init() {
+void init()
+{
     freopen("Input.inp", "r", stdin);
     freopen("Output.out", "w", stdout);
 }
 
-void quickSort(int a[], int l, int r) {
+void quickSort(int a[], int l, int r)
+{
     int i = l, j = r;
     int pivot = a[(l + r) / 2];
-    while (i <= j) {
-        while (a[i] < pivot) ++i;
-        while (a[j] > pivot) --j;
-        if (i <= j) {
+    while (i <= j)
+    {
+        while (a[i] < pivot)
+            ++i;
+        while (a[j] > pivot)
+            --j;
+        if (i <= j)
+        {
             int tmp = a[i];
             a[i] = a[j];
             a[j] = tmp;
@@ -20,23 +26,30 @@ void quickSort(int a[], int l, int r) {
             --j;
         }
     }
-    if (l < j) quickSort(a, l, j);
-    if (i < r) quickSort(a, i, r);
+    if (l < j)
+        quickSort(a, l, j);
+    if (i < r)
+        quickSort(a, i, r);
 }
 
 long long d;
 int a[100001], n, k, res = 0;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin >> n >> d >> k;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    while (d > 0 && ++res <= k) {
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    while (d > 0 && ++res <= k)
+    {
         quickSort(a, 1, n);
         d -= a[n];
         a[n] /= 2;
     }
-    if (res <= k && d <= 0) cout << res;
-    else cout << -1;
+    if (res <= k && d <= 0)
+        cout << res;
+    else
+        cout << -1;
     return 0;
 }
