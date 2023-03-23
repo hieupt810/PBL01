b125437790acc2368b460b8b8e29075f396637ac
hieupt <trunghieupham03@gmail.com>
2023-03-23 13:31:10 +0700

    #

--- algorithm/max_heap.cpp
+++ algorithm/max_heap.cpp
@@ -2,12 +2,6 @@
 #include <fstream>
 using namespace std;
 
-void init()
-{
-    freopen("Input.inp", "r", stdin);
-    freopen("Output.out", "w", stdout);
-}
-
 void max_heapify(int a[], int i, int n)
 {
     int left = 2 * i, right = 2 * i + 1, largest;
@@ -38,20 +32,23 @@ int a[100001], n, k, res = 0;
 int main()
 {
     ios_base::sync_with_stdio(0);
-    init();
-    cin >> n >> d >> k;
+    freopen("Input.inp", "r", stdin);
+    // cin >> n >> d >> k;
+    cin >> n;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
     build_maxheap(a, n);
-    while (d > 0 && ++res <= k + 1)
-    {
-        d -= a[1];
-        a[1] /= 2;
-        max_heapify(a, 1, n);
-    }
-    if (res <= k && d <= 0)
-        cout << res;
-    else
-        cout << -1;
+    // while (d > 0 && ++res <= k + 1)
+    // {
+    //     d -= a[1];
+    //     a[1] /= 2;
+    //     max_heapify(a, 1, n);
+    // }
+    // if (res <= k && d <= 0)
+    //     cout << res;
+    // else
+    //     cout << -1;
+    for (int i = 1; i <= n; i++)
+        cout << a[i] << " ";
     return 0;
 }
